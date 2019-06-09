cecc4c5bedc68158f614706a51976391b5772881
Tyler Gragg <robots@tylergragg.com>
2019-06-08 21:43:39 -0400

    updated dependencies

--- install_scripts/install_deps.sh
+++ install_scripts/install_deps.sh
@@ -9,6 +9,7 @@ sudo pip3 install -U pip
 sudo apt-get install zlib1g-dev zip libjpeg8-dev libhdf5-dev hdf5-tools
 sudo pip3 install -U numpy grpcio absl-py py-cpuinfo psutil portpicker grpcio six mock requests gast h5py astor termcolor jupyter
 sudo pip3 install protobuf==3.6.1
+sudo pip3 install pandas
 
 sudo pip3 install --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 tensorflow-gpu==1.13.1+nv19.4
 
